4aab153a6aef4bdb88eda2ed98b9c1c807eefd06
PedroGuidotte <peguidotte@gmail.com>
2024-10-02 12:43:22 -0300

    nav mobile concluida

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ audio.loop = true;
 const menuIcon = document.querySelector('#menu-icon');
 const navbarNav = document.querySelector('#navbarNav');
 
-btnMusica.addEventListener('click', () => {
+let lastScrollTop = 0;
+const navbar = document.querySelector('nav');
+let scrollTimeout;
+
+btnMusica.addEventListener('click', (e) => {
+    e.preventDefault();
     if (audio.paused) {
         audio.play();
         btnMusica.innerHTML = '<i class="fa-solid fa-pause"></i>';
@@ -23,4 +28,18 @@ navbarNav.addEventListener('shown.bs.collapse', () => {
 navbarNav.addEventListener('hidden.bs.collapse', () => {
     menuIcon.classList.remove('fa-times');
     menuIcon.classList.add('fa-bars');
+});
+
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const navbarHeight = navbar.offsetHeight;
+
+        window.scrollTo({
+            top: targetElement.offsetTop - navbarHeight/3,
+            behavior: 'smooth'
+        });
+    });
 });
