acfa9058d28461a80aa67450733b0dffda0202c5
Gabriel Vara <gabriel.alvesvara@outlook.com>
2024-10-02 18:50:51 -0300

    Corrigindo Erros

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ btnMusica.addEventListener('click', (e) => {
     e.preventDefault();
     if (audio.paused) {
         audio.play();
-        btnMusica.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        btnMusica.innerHTML = '<i class="fa-solid fa-pause responsive-icon-music"></i>';
     } else {
         audio.pause();
-        btnMusica.innerHTML = '<i class="fa-solid fa-music"></i>';
+        btnMusica.innerHTML = '<i class="fa-solid fa-music responsive-icon-music"></i>';
     }
 })
 
@@ -43,3 +43,50 @@ document.querySelectorAll('a.nav-link').forEach(anchor => {
         });
     });
 });
+
+function adjustCarouselImages() {
+    const width = window.innerWidth;
+
+    const images = {
+        mobile: [
+            "assets/cards/M-Cadastro.svg",
+            "assets/cards/M-Navegue=.svg",
+            "assets/cards/M-Crie.svg",
+            "assets/cards/M-Escute.svg"
+        ],
+        tablet: [
+            "assets/cards/T-Cadastro.svg", 
+            "assets/cards/T-Navegue.svg",
+            "assets/cards/T-Crie.svg",
+            "assets/cards/T-Escute.svg"
+        ],
+        desktop: [
+            "assets/cards/D-Cadastre.svg", 
+            "assets/cards/D-Navegue.svg",
+            "assets/cards/D-Crie.svg",
+            "assets/cards/D-Escute.svg"
+        ]
+    };
+
+    // Determine qual conjunto de imagens usar
+    let currentImages;
+    if (width < 768) { // Mobile
+        currentImages = images.mobile;
+    } else if (width >= 768 && width < 992) { // Tablet
+        currentImages = images.tablet;
+    } else { // Desktop
+        currentImages = images.desktop;
+    }
+
+    // Atualize o atributo src das imagens
+    document.getElementById('image-1').src = currentImages[0];
+    document.getElementById('image-2').src = currentImages[1];
+    document.getElementById('image-3').src = currentImages[2];
+    document.getElementById('image-4').src = currentImages[3];
+}
+
+// Chamar a função no carregamento da página
+window.onload = adjustCarouselImages;
+
+// Chamar a função ao redimensionar a janela
+window.onresize = adjustCarouselImages;
